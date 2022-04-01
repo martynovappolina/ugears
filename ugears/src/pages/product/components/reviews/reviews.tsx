586b4c066fd18fe24bda09ed11d0bf9f5312ca2d
martynovappolina <polinagalaxytab27@gmail.com>
2022-04-01 03:33:53 +0300

    reviews

--- ugears/src/pages/product/components/reviews/reviews.tsx
+++ ugears/src/pages/product/components/reviews/reviews.tsx
@@ -1,7 +1,8 @@
 import Rating from "@components/rating";
+import { ReviewModel } from "@store/models/Reviews";
 import ReviewsStore from "@store/ReviewsStore";
-import { useLocalStore } from "mobx-react-lite";
-import { useEffect } from "react";
+import { observer, useLocalStore } from "mobx-react-lite";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import reviewsStyle from './reviews.module.scss'
@@ -11,10 +12,21 @@ const Reviews = () => {
     const reviewsStore = useLocalStore(() => new ReviewsStore());
     useEffect(() => {reviewsStore.getReviewsByProductId({id: id})}, []); 
 
+    const [allRev, setAllRev] = useState(1)
+    // 1-больше трех отзывов, выводятся первые 3
+    // 2-выводятся все отзывы
+    
+    const showRev = () => setAllRev(2);
+    const hideRev = () => setAllRev(1);
+
+    var reviews: ReviewModel[];
+    if ((reviewsStore.list.length<3)||(allRev===2)) reviews = reviewsStore.list;
+    else reviews = reviewsStore.list.slice(0,3);
+console.log(allRev)
     if(reviewsStore.list.length !== 0) return (
         <div className={reviewsStyle.reviews}>
             {
-                reviewsStore.list.map((rev) => 
+                reviews.map((rev) => 
                     <div key={rev.userId} className={reviewsStyle.review}>
                         <div className={reviewsStyle.review__User}>
                             <Rating stars={rev.rating}/>
@@ -27,9 +39,12 @@ const Reviews = () => {
                     </div>
                 )
             }
+            {reviewsStore.list.length<3? null: allRev===1? 
+            <div onClick={showRev} className={reviewsStyle.reviews__Link}>Показать все отзывы</div>: 
+            <div onClick={hideRev} className={reviewsStyle.reviews__Link}>Скрыть отзывы</div>}
         </div>
     )
     return null
 };
 
-export default Reviews;
+export default observer(Reviews);
