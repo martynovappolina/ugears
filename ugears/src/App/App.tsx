504a60dc72bd2e9fdb7c992042d8d2ff8bae2f61
martynovappolina <polinagalaxytab27@gmail.com>
2022-04-03 04:07:50 +0300

    gears

--- ugears/src/App/App.tsx
+++ ugears/src/App/App.tsx
@@ -5,9 +5,10 @@ import { Route, BrowserRouter, Redirect } from 'react-router-dom';
 import appStyle from './App.module.scss'
 import Header from "./components/header";
 import { useLocalStore } from "mobx-react-lite";
-import React from "react";
+import React, { useState } from "react";
 import Authorization from "@pages/authorization";
 import UserStore from "@store/UserStore";
+import Gears from "./components/gears";
 
 
 const UserContext = React.createContext({
@@ -19,18 +20,26 @@ export const useUserContext = () => React.useContext(UserContext);
 const App = () => {
 
   const userStore = useLocalStore(() => new UserStore());
-  
+  const [rotateGearP, setRotateGearP] = useState(0);
+  const [rotateGearM, setRotateGearM] = useState(0);
+
+  const rotate = () => {
+    setRotateGearP(rotateGearP + 5);
+    setRotateGearM(rotateGearM - 5);
+  }
+
   return (
-    <div className={appStyle.body}>
-        <Provider value={{ userStore }}>
-          <BrowserRouter>
-            <Header />
-            <Route path="/products" component={Catalog} />
-            <Route path="/product/:id" component={Product} />
-            <Route path="/autho" component={Authorization} />
-            <Redirect to="/"/>
-          </BrowserRouter>
-        </Provider>
+    <div className={appStyle.body} onWheel={rotate}>
+      <Gears rotateGearP={rotateGearP} rotateGearM={rotateGearM}/>
+      <Provider value={{ userStore }}>
+        <BrowserRouter>
+          <Header />
+          <Route path="/products" component={Catalog} />
+          <Route path="/product/:id" component={Product} />
+          <Route path="/autho" component={Authorization} />
+          <Redirect to="/"/>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
